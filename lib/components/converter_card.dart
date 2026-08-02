f4c4072e256aa16eb33ea08073b98a6030ca7f47
Mauro Pot <120337162+MauroPot2@users.noreply.github.com>
2026-08-02 12:38:16 +0200

    Restore shared element helper import

--- lib/components/converter_card.dart
+++ lib/components/converter_card.dart
@@ -1,5 +1,6 @@
 import 'package:jaspr/dom.dart';
 import 'package:jaspr/jaspr.dart';
+import 'package:potube_web/components/site_scaffold.dart';
 
 class ConverterCard extends StatelessComponent {
   const ConverterCard({super.key});
