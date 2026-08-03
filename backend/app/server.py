6534b628d65234cd74ef0bfba7ee2ca9180dbc05
Mauro Pot <120337162+MauroPot2@users.noreply.github.com>
2026-08-03 17:29:07 +0200

    Use package-relative backend imports

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,4 +1,4 @@
-from app.main import app
-from app.youtube_case_study import router as youtube_case_study_router
+from .main import app
+from .youtube_case_study import router as youtube_case_study_router
 
 app.include_router(youtube_case_study_router)
