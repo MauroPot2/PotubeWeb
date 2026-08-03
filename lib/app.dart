dba8d5cebd873332212183ea15bc67ba1c6c9113
Mauro Pot <120337162+MauroPot2@users.noreply.github.com>
2026-08-03 17:23:49 +0200

    Register hidden case study route

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,7 @@ import 'package:potube_web/pages/copyright_page.dart';
 import 'package:potube_web/pages/home_page.dart';
 import 'package:potube_web/pages/privacy_page.dart';
 import 'package:potube_web/pages/terms_page.dart';
+import 'package:potube_web/pages/youtube_case_study_page.dart';
 
 class App extends StatelessComponent {
   const App({super.key});
@@ -18,6 +19,10 @@ class App extends StatelessComponent {
         Route(path: '/terms', builder: (context, state) => const TermsPage()),
         Route(path: '/copyright', builder: (context, state) => const CopyrightPage()),
         Route(path: '/guide/mp3-bitrate', builder: (context, state) => const ArticleBitratePage()),
+        Route(
+          path: '/lab/youtube-audio-case-study',
+          builder: (context, state) => const YoutubeCaseStudyPage(),
+        ),
       ],
     );
   }
