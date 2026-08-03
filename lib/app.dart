cb4c94ebc154b4ebfdb767fb7915bac87e794ccb
Mauro Pot <120337162+MauroPot2@users.noreply.github.com>
2026-08-03 17:50:21 +0200

    Expand public guide routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,9 @@
 import 'package:jaspr/jaspr.dart';
 import 'package:jaspr_router/jaspr_router.dart';
 import 'package:potube_web/pages/article_bitrate_page.dart';
+import 'package:potube_web/pages/article_normalization_page.dart';
 import 'package:potube_web/pages/copyright_page.dart';
+import 'package:potube_web/pages/guides_page.dart';
 import 'package:potube_web/pages/home_page.dart';
 import 'package:potube_web/pages/privacy_page.dart';
 import 'package:potube_web/pages/terms_page.dart';
@@ -15,10 +17,21 @@ class App extends StatelessComponent {
     return Router(
       routes: [
         Route(path: '/', builder: (context, state) => const HomePage()),
+        Route(path: '/guide', builder: (context, state) => const GuidesPage()),
+        Route(
+          path: '/guide/mp3-bitrate',
+          builder: (context, state) => const ArticleBitratePage(),
+        ),
+        Route(
+          path: '/guide/normalizzazione-audio',
+          builder: (context, state) => const ArticleNormalizationPage(),
+        ),
         Route(path: '/privacy', builder: (context, state) => const PrivacyPage()),
         Route(path: '/terms', builder: (context, state) => const TermsPage()),
-        Route(path: '/copyright', builder: (context, state) => const CopyrightPage()),
-        Route(path: '/guide/mp3-bitrate', builder: (context, state) => const ArticleBitratePage()),
+        Route(
+          path: '/copyright',
+          builder: (context, state) => const CopyrightPage(),
+        ),
         Route(
           path: '/lab/youtube-audio-case-study',
           builder: (context, state) => const YoutubeCaseStudyPage(),
